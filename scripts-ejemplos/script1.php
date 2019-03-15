5bb2356900e3789a09206eadccebcc8f6701ef85
pantojamao <mauriciop173@hotmail.com>
2019-03-15 12:05:07 -0500

    nuevo

---
+++ scripts-ejemplos/script1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<CENTER> PROGRAMACION</CENTER>;
+<html>
+
+<head>
+
+
+
+
+
+</head>
+
+<body>
+
+<?php
+
+$nombre= "peter";
+$apellido= "watson";
+$edad=30;
+$salario=800;
+
+echo "<center> BIENVENIDOS<br>";
+echo "nombre :".$salario."<br>";
+echo "apellido :".$apellido;
+echo "<br> edad:".$edad;
+echo "<br>salario".$salario;
+
+?>
+
+</body>
+
+</html>
---
+++ scripts-ejemplos/script2.php
@@ -0,0 +1,17 @@
+<?php
+$numero1=10;
+$numero2=5;
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+echo "<center>";
+echo "la suma Es:".$suma;
+echo "<br>la multiplicacion Es".$resta;
+echo "<br>la resta  Es:".$multiplicacion;
+echo "<br>la divion  Es:".$division;
+echo"</center>";
+
+
+
+?>
